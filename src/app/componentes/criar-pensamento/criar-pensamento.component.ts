3cae5cf1eab340bce290f9116c8d8dfc095a01f1
unknown <joaoantoniofer@gmail.com>
2022-10-25 16:55:09 -0300

    First commit 16:49 25/10/22

---
+++ src/app/componentes/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent implements OnInit {
+
+  pensamento = {id:'1', conteudo:'Aprender conteudo',autoria:'Dev',modelo:'modelo1'}
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  criarPensamento(){
+    alert("Novo pensamento criado")
+  }
+
+  cancelar(){
+    alert("cancelado")
+  }
+}
